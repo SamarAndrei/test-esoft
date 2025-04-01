1d60c8dfe38b3c2ce0dd4669da8518a0e5545879
Andrei Samar <istore103@icloud.com>
2025-04-01 16:48:09 +0500

    fixDeploy

--- backend/build/db.js
+++ backend/build/db.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const knex_1 = __importDefault(require("knex"));
-const knexfile_1 = __importDefault(require("../src/knexfile"));
+const knexfile_1 = __importDefault(require("./knexfile"));
 const db = (0, knex_1.default)(knexfile_1.default.development);
 exports.default = db;
