16c8a705747650611f513ec3a882c4368df6095c
Andrei Samar <istore103@icloud.com>
2025-04-01 20:17:54 +0500

    fixBuild

--- backend/build/knexfile.js
+++ backend/build/knexfile.js
@@ -5,11 +5,11 @@ const config = {
     development: {
         client: 'pg',
         connection: {
-            host: process.env.DB_HOST || 'localhost',
+            host: process.env.DB_HOST || 'dpg-cvltdva4d50c73e7htng-a',
             port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-            user: process.env.DB_USER || 'postgres',
-            password: process.env.DB_PASSWORD || 'postgres',
-            database: process.env.DB_DATABASE || 'esoft_test',
+            user: process.env.DB_USER || 'postgres_esoft_test_user',
+            password: process.env.DB_PASSWORD || 'iPaGX6au8jlrFaUz5hC5RjkzbXZkmTlm',
+            database: process.env.DB_DATABASE || 'postgres_esoft_test',
         },
         pool: {
             min: process.env.DB_MIN ? parseInt(process.env.DB_MIN) : 2,
